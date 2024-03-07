3e2ddf44ea1be6b9fdf5343b02d668ad96cb9c15
Eloy <eloyor7@gmail.com>
2024-03-07 09:40:03 +0100

    Cambio Fch Fecha

---
+++ src/test/kotlin/FechaTest.kt
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class FechaTest {
+
+    @Test
+    fun valida() {
+
+        @Test
+        fun valida() {
+            val fch = Fecha(15, 13, 2023)
+            assertTrue (fch.valida())
+        }
+
+
+        @Test
+        fun fechaInvalidaDia() {
+            val fch = Fecha(32, 5, 2022)
+            assertFalse (fch.valida())
+        }
+
+        @Test
+        fun fechaInvalidaMes() {
+            val fch = Fecha(12, 13, 2021)
+            // Assert.assertFalse(fecha.valida())
+            assertFalse (fch.valida())
+        }
+
+        @Test
+        fun fechaInvalidaFebreroBisiesto() {
+            val fch = Fecha(29, 2, 2021)
+            assertFalse (fch.valida())
+            println("Fecha invalida")
+            println(fch.valida())
+        }
+
+        @Test
+        fun fechaValidaFebreroBisiesto() {
+            val Fecha = Fecha(29, 2, 2024)
+            assertTrue (Fecha.valida())
+            println("Fecha valida Bisiesto")
+            println(Fecha.valida())
+        }
+    }
+}
