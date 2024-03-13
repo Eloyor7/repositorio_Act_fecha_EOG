2117d5748693bc83c6b96b6db6818c7dfd2a42cc
Eloyor7 <eloyor7@gmail.com>
2024-03-13 20:51:42 +0100

    Refactorización finaal

--- src/main/kotlin/Fecha.kt
+++ src/main/kotlin/Fecha.kt
@@ -2,8 +2,7 @@ class Fecha(private val dia: Int, private val mes: Int, private val a: Int) {
 
     fun valida(): Boolean {
 
-        if (dia < 1 || dia > 31) return false
-        if (mes < 1 || mes > 12) return false
+        if ((dia < 1 || dia > 31) || (mes < 1 || mes > 12)) return false
 
         // determinamos la cantidad de días del mes:
         return diasdelMes()
@@ -16,6 +15,6 @@ class Fecha(private val dia: Int, private val mes: Int, private val a: Int) {
             4, 6, 9, 11 -> diasMes = 30
             2 -> diasMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
         }
-        return if (dia > diasMes) false else true
+        return dia <= diasMes
     }
 }
