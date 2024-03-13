6d0d565e4aa7fe97314b3048caf55af4a0ab252b
Eloyor7 <eloyor7@gmail.com>
2024-03-13 20:37:36 +0100

    test

---
+++ src/main/kotlin/Fecha.kt
@@ -0,0 +1,36 @@
+/**
+ * Fecha
+ *
+ * @author Eloy Ormeño
+ *
+ * @property dia
+ * @property mes
+ * @property a
+ * @constructor Create empty Fecha
+ */
+class Fecha(private val dia: Int, private val mes: Int, private val a: Int) {
+
+    /**
+     * Valida
+     *
+     * @return diasdelMes
+     */
+    fun valida(): Boolean {
+
+        if (dia < 1 || dia > 31) return false
+        if (mes < 1 || mes > 12) return false
+
+        // determinamos la cantidad de días del mes:
+        return diasdelMes()
+    } // … más métodos
+
+    private fun diasdelMes(): Boolean {
+        var diasMes = 0
+        when (mes) {
+            1, 3, 5, 7, 8, 10, 12 -> diasMes = 31
+            4, 6, 9, 11 -> diasMes = 30
+            2 -> diasMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
+        }
+        return if (dia > diasMes) false else true
+    }
+}
---
+++ src/test/kotlin/FechaTest.kt
@@ -0,0 +1,87 @@
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+/**
+ * Fecha test
+ *
+ * @author Eloy Ormeño
+ *
+ * @constructor Create empty Fecha test
+ */
+class FechaTest {
+
+    /**
+     * Set up
+     *
+     */
+    @BeforeEach
+    fun setUp() {
+    }
+
+    /**
+     * Tear down
+     *
+     */
+    @AfterEach
+    fun tearDown() {
+    }
+
+    /**
+     * Valida
+     *
+     */
+    @Test
+    fun valida() {
+        val fecha = Fecha(15, 12, 2023)
+        assertTrue (fecha.valida())
+    }
+
+
+    /**
+     * Fecha invalida dia
+     *
+     */
+    @Test
+    fun fechaInvalidaDia() {
+        val fecha = Fecha(32, 5, 2022)
+        assertFalse (fecha.valida())
+    }
+
+    /**
+     * Fecha invalida mes
+     *
+     */
+    @Test
+    fun fechaInvalidaMes() {
+        val fecha = Fecha(12, 13, 2021)
+        // Assert.assertFalse(fecha.valida())
+        assertFalse (fecha.valida())
+    }
+
+    /**
+     * Fecha invalida febrero bisiesto
+     *
+     */
+    @Test
+    fun fechaInvalidaFebreroBisiesto() {
+        val fecha = Fecha(29, 2, 2021)
+        assertFalse (fecha.valida())
+        println("Fecha invalida")
+        println(fecha.valida())
+    }
+
+    /**
+     * Fecha valida febrero bisiesto
+     *
+     */
+    @Test
+    fun fechaValidaFebreroBisiesto() {
+        val fecha = Fecha(29, 2, 2024)
+        assertTrue (fecha.valida())
+        println("Fecha valida Bisiesto")
+        println(fecha.valida())
+    }
+
+}
